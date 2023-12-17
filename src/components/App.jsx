70af1ef817ad08bd0fe5a569766c1f11a8c83874
BendelVolodymyr <bendel.vova@gmail.com>
2023-12-17 12:10:19 +0100

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import Section from './Section/Section';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import contactsBook from '../data/contacts.json';
 
 const LOCAL_KEY_CONTACTS = 'contacts';
-// const LOCAL_KEY_FILTER = 'filter';
 
 const notifyOptions = {
   position: 'top-left',
@@ -19,57 +19,27 @@ const notifyOptions = {
   progress: undefined,
   theme: 'colored',
 };
+//
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+// const handlerInputData = data => {
 
-  componentDidMount() {
-    const getLocalContacts = localStorage.getItem(LOCAL_KEY_CONTACTS);
-    const validKeyContact = getLocalContacts === null;
+//   return console.log(data);
+//   // this.handlerResultChange(data);
+// };
 
-    if (!validKeyContact) {
-      const parseLocalContacts = JSON.parse(getLocalContacts);
-      this.setState({ contacts: parseLocalContacts });
-    }
+export default function App() {
+  const [contacts, setContacts] = useState(
+    JSON.parse(window.localStorage.getItem(LOCAL_KEY_CONTACTS)) ?? contactsBook
+  );
+  const [filter, setFilter] = useState('');
 
-    // const getLocalFilter = localStorage.getItem(LOCAL_KEY_FILTER);
-    // const validKeyFilter = getLocalFilter === null;
+  useEffect(() => {
+    window.localStorage.setItem(LOCAL_KEY_CONTACTS, JSON.stringify(contacts));
+  }, [contacts]);
 
-    // if (!validKeyFilter) {
-    //   const parseLocalFilter = JSON.parse(getLocalFilter);
-    //   this.setState({ filter: parseLocalFilter });
-    // }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    const validResultState =
-      this.state.contacts.length !== prevState.contacts.length;
-
-    // const validFilter = this.state.filter.length !== prevState.filter.length;
-
-    if (validResultState) {
-      localStorage.setItem(
-        LOCAL_KEY_CONTACTS,
-        JSON.stringify(this.state.contacts)
-      );
-    }
-
-    // if (validFilter) {
-    //   localStorage.setItem(LOCAL_KEY_FILTER, JSON.stringify(this.state.filter));
-    // }
-  }
-
-  handlerResultChange(data) {
+  const handlerResultChange = data => {
     const { name, number } = data;
-    const validInput = this.state.contacts.some(function (element) {
+    const validInput = contacts.some(function (element) {
       return (
         element.name.toLowerCase().trim() === name.toLowerCase().trim() ||
         element.number.trim() === number.trim()
@@ -77,58 +47,141 @@ export class App extends Component {
     });
 
     return validInput
-      ? toast.info(`${name}: is already in contacts `, notifyOptions)
-      : this.setState(prevState => {
-          return {
-            contacts: [data, ...prevState.contacts],
-          };
-        });
-  }
-
-  handlerInputData = data => {
-    this.handlerResultChange(data);
+      ? toast.info(
+          `${name} or phone ${number}: is already in contacts `,
+          notifyOptions
+        )
+      : setContacts([data, ...contacts]);
   };
 
-  handlerFilterChange = event => {
-    this.setState({ filter: event.currentTarget.value.toLowerCase().trim() });
+  const handlerFilterChange = event => {
+    setFilter(event.currentTarget.value.toLowerCase().trim());
   };
 
-  visibleContacts = () => {
-    const { filter, contacts } = this.state;
+  const visibleContacts = () => {
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter)
     );
   };
 
-  removeContact = id => {
-    this.setState(prevState => {
-      return {
-        contacts: prevState.contacts.filter(contact => contact.id !== id),
-      };
-    });
+  const removeContact = id => {
+    const result = contacts.filter(contact => contact.id !== id);
+    setContacts(result);
   };
 
-  render() {
-    const { filter } = this.state;
-    const createContactList = this.visibleContacts();
-    return (
-      <>
-        <Section title="Phonebook">
-          <ContactForm onSubmit={this.handlerInputData} />
-        </Section>
-        <Section title="Contacts">
-          <Filter
-            title="Find contacts by name"
-            value={filter}
-            onChange={this.handlerFilterChange}
-          />
-          <ToastContainer />
-          <ContactList
-            createList={createContactList}
-            onDelete={this.removeContact}
-          />
-        </Section>
-      </>
-    );
-  }
+  return (
+    <>
+      <Section title="Phonebook">
+        <ContactForm onSubmit={handlerResultChange} />
+      </Section>
+      <Section title="Contacts">
+        <Filter
+          title="Find contacts by name"
+          value={filter}
+          onChange={handlerFilterChange}
+        />
+        <ToastContainer />
+        <ContactList createList={visibleContacts()} onDelete={removeContact} />
+      </Section>
+    </>
+  );
 }
+
+// export class App extends Component {
+//   state = {
+//     contacts: [
+//       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+//     ],
+//     filter: '',
+//   };
+
+//   componentDidMount() {
+//     const getLocalContacts = localStorage.getItem(LOCAL_KEY_CONTACTS);
+//     const validKeyContact = getLocalContacts === null;
+
+//     if (!validKeyContact) {
+//       const parseLocalContacts = JSON.parse(getLocalContacts);
+//       this.setState({ contacts: parseLocalContacts });
+//     }
+//   }
+
+//   componentDidUpdate(prevProps, prevState) {
+//     const validResultState =
+//       this.state.contacts.length !== prevState.contacts.length;
+
+//     if (validResultState) {
+//       localStorage.setItem(
+//         LOCAL_KEY_CONTACTS,
+//         JSON.stringify(this.state.contacts)
+//       );
+//     }
+//   }
+
+//   handlerResultChange(data) {
+//     const { name, number } = data;
+//     const validInput = this.state.contacts.some(function (element) {
+//       return (
+//         element.name.toLowerCase().trim() === name.toLowerCase().trim() ||
+//         element.number.trim() === number.trim()
+//       );
+//     });
+
+//     return validInput
+//       ? toast.info(`${name}: is already in contacts `, notifyOptions)
+//       : this.setState(prevState => {
+//           return {
+//             contacts: [data, ...prevState.contacts],
+//           };
+//         });
+//   }
+
+//   handlerInputData = data => {
+//     this.handlerResultChange(data);
+//   };
+
+//   handlerFilterChange = event => {
+//     this.setState({ filter: event.currentTarget.value.toLowerCase().trim() });
+//   };
+
+//   visibleContacts = () => {
+//     const { filter, contacts } = this.state;
+//     return contacts.filter(contact =>
+//       contact.name.toLowerCase().includes(filter)
+//     );
+//   };
+
+//   removeContact = id => {
+//     this.setState(prevState => {
+//       return {
+//         contacts: prevState.contacts.filter(contact => contact.id !== id),
+//       };
+//     });
+//   };
+
+//   render() {
+//     const { filter } = this.state;
+//     const createContactList = this.visibleContacts();
+//     return (
+//       <>
+//         <Section title="Phonebook">
+//           <ContactForm onSubmit={this.handlerInputData} />
+//         </Section>
+//         <Section title="Contacts">
+//           <Filter
+//             title="Find contacts by name"
+//             value={filter}
+//             onChange={this.handlerFilterChange}
+//           />
+//           <ToastContainer />
+//           <ContactList
+//             createList={createContactList}
+//             onDelete={this.removeContact}
+//           />
+//         </Section>
+//       </>
+//     );
+//   }
+// }
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,72 +1,111 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import shortid from 'shortid';
+
+export default function ContactForm({ onSubmit }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handlerInputChange = event => {
+    const { name, value } = event.target;
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+
+      case 'number':
+        setNumber(value);
+        break;
+
+      default:
+        return;
+    }
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const id = shortid();
+    const { name, number } = event.currentTarget;
+    const resultSubmit = { name: name.value, number: number.value, id: id };
+    onSubmit(resultSubmit);
+  };
+
+  return (
+    <form className="form__box" onSubmit={handleSubmit}>
+      <label>
+        Name
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={handlerInputChange}
+          required
+        />
+      </label>
+      <label>
+        Number
+        <input
+          type="tel"
+          name="number"
+          value={number}
+          onChange={handlerInputChange}
+          required
+        />
+      </label>
+      <button type="submit">Add Contact</button>
+    </form>
+  );
+}
 
 // const LOCAL_KEY = 'inputValue';
 
-class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
-    id: '',
-  };
+// class ContactForm extends Component {
+//   state = {
+//     name: '',
+//     number: '',
+//     id: '',
+//   };
 
-  componentDidMount() {
-    // const getLocal = localStorage.getItem(LOCAL_KEY);
-    // const validKey = getLocal === null;
-    // if (!validKey) {
-    //   const parseLocal = JSON.parse(getLocal);
-    //   const { name, number } = parseLocal;
-    //   this.setState({ name: name, number: number });
-    // }
-  }
+//   componentDidMount() {}
 
-  // componentDidUpdate(prevProps, prevState) {
-  //   if (this.name !== prevState.name || this.number !== prevState.number) {
-  //     localStorage.setItem(LOCAL_KEY, JSON.stringify(this.state));
-  //   }
-  // }
+//   reset = () => {
+//     this.setState({ name: '', number: '', id: '' });
+//   };
 
-  reset = () => {
-    this.setState({ name: '', number: '', id: '' });
-  };
+//   handlerInputChange = event => {
+//     const { name, value } = event.currentTarget;
+//     this.setState({ id: shortid(), [name]: value });
+//   };
 
-  handlerInputChange = event => {
-    const { name, value } = event.currentTarget;
-    this.setState({ id: shortid(), [name]: value });
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-    this.props.onSubmit(this.state);
-    this.reset();
-  };
-  render() {
-    return (
-      <form className="form__box" onSubmit={this.handleSubmit}>
-        <label>
-          Name
-          <input
-            type="text"
-            name="name"
-            value={this.state.name}
-            onChange={this.handlerInputChange}
-            required
-          />
-        </label>
-        <label>
-          Number
-          <input
-            type="tel"
-            name="number"
-            value={this.state.number}
-            onChange={this.handlerInputChange}
-            required
-          />
-        </label>
-        <button type="submit">Add Contact</button>
-      </form>
-    );
-  }
-}
-
-export default ContactForm;
+//   handleSubmit = event => {
+//     event.preventDefault();
+//     this.props.onSubmit(this.state);
+//     this.reset();
+//   };
+//   render() {
+//     return (
+//       <form className="form__box" onSubmit={this.handleSubmit}>
+//         <label>
+//           Name
+//           <input
+//             type="text"
+//             name="name"
+//             value={this.state.name}
+//             onChange={this.handlerInputChange}
+//             required
+//           />
+//         </label>
+//         <label>
+//           Number
+//           <input
+//             type="tel"
+//             name="number"
+//             value={this.state.number}
+//             onChange={this.handlerInputChange}
+//             required
+//           />
+//         </label>
+//         <button type="submit">Add Contact</button>
+//       </form>
+//     );
+//   }
+// }
